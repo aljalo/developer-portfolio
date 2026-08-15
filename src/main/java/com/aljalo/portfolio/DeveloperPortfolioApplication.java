ec36d8c6f221efe0fdfa167c2cd0a9dfe4c19e0f
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-08-15 18:53:47 +0300

    chore: initialize Spring Boot portfolio

---
+++ src/main/java/com/aljalo/portfolio/DeveloperPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.aljalo.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aljalo/portfolio/DeveloperPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljalo.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
